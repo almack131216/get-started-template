8b2b87113bf4c86cf7f27e03c79956ae931706be
ecp-alex <alex.mackenzie@englishcollege.cz>
2022-04-11 13:17:09 +0200

    FUNC | add 'open/close all' btns to accordion

--- src/components/Accordion/AccordionItem/index.js
+++ src/components/Accordion/AccordionItem/index.js
@@ -1,21 +1,26 @@
-import React, { useState } from "react"
+import React, { useEffect, setState, useState } from "react"
 // import { FaPlus, FaMinus } from 'react-icons/fa'
 
-const AccordionItem = ({ title, image_url, children }) => {
+const AccordionItem = ({ title, image_url, children, isExpanded }) => {
   const [expanded, setExpanded] = useState(false)
+
+  useEffect(() => {
+    console.log("allExpanded: ", isExpanded)
+    setExpanded(isExpanded)
+  }, [isExpanded])
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
 
   return (
     <article className='question'>
-      <header onClick={() => setExpanded(!expanded)}>
+      <header onClick={() => toggleExpanded()}>
         <div className='image-container'>
           <img src={image_url} alt={title} />
         </div>
-        <h4 className='question-title'>
-          {title}
-        </h4>
-        <button className='btn'>
-          {expanded ? "-" : "+"}
-        </button>
+        <h4 className='question-title'>{title}</h4>
+        <button className='btn'>{expanded ? "-" : "+"}</button>
       </header>
       {expanded && <div className='answer'>{children}</div>}
     </article>
--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react"
 import AccordionItem from "./AccordionItem"
 
 const Accordion = ({ header, items }) => {
+  const [allExpanded, setAllExpanded] = useState(false)
+
   return (
     <>
       {header ? <h1>{header}</h1> : ""}
 
+      {/* <p>status: {allExpanded ? "yes" : "no"}</p> */}
+      <button onClick={() => setAllExpanded(true)}>open all</button>
+      <button onClick={() => setAllExpanded(false)}>close all</button>
+      
       <div className='accordion-container'>
         {items.map((item) => (
           <AccordionItem
             key={item.id}
             title={item.name}
             image_url={item.image_url}
+            isExpanded={allExpanded}
           >
             {item.tagline && (
               <blockquote className='blockquote'>

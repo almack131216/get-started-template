45f286aac317571caadba4e6c68beacb8115eb99
ecp-alex <alex.mackenzie@englishcollege.cz>
2022-04-11 18:39:37 +0200

    CR | add comments

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -3,12 +3,14 @@ import Card from "../Card"
 const Grid = ({ header, items }) => {
   return (
     <>
-      {header ? <h1>{header}</h1> : ""}
-      <div className='grid-container'>
-        {items.map((item) => (
-          <Card key={item.id} item={item} />
-        ))}
-      </div>
+      {header ? <h1>{header}</h1> : null}
+      {items ? (
+        <div className='grid-container'>
+          {items.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      ) : null}
     </>
   )
 }
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -5,7 +5,18 @@ const AboutPage = () => {
     document.title = "About"
   }, [])
 
-  return <div className='page-container'>AboutPage</div>
+  return (
+    <div className='page-container'>
+      <h1>About</h1>
+      <strong>With more time...</strong>
+      <ul>
+        <li>Bugfix for saving to/loading from sessionStorage</li>
+        <li>Better UI – perhaps using Tailwind</li>
+        <li>Sort functionality</li>
+        <li>Better comments</li>
+      </ul>
+    </div>
+  )
 }
 
 export default AboutPage
